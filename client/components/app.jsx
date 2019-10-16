83a5f29272f5e1354d22857a8f4bd574e0c50d4b
Christie Hui <cwsh15@gmail.com>
2019-10-16 13:27:36 -0700

    Merge pull request #6 from chui15/title-page
    completed first draft of intro screen and added pathway to home screen

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,20 +1,40 @@
 import React from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import IntroScreen from './intro-screen';
+import HomeScreen from './home-screen';
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      view: {
+        name: 'intro',
+        params: {}
+      }
+    };
+    this.setView = this.setView.bind(this);
+  }
+
+  setView(name, params){
+    this.setState({
+      view: {
+        name: name,
+        params: params
+      }
+    });
+  }
 
   render(){
+    let screen;
+    if (this.state.view.name === 'intro'){
+      screen = <IntroScreen setView={this.setView} />;
+    } else if (this.state.view.name === 'home'){
+      screen = <HomeScreen />;
+    }
     return (
-      <Router>
-      <div className="title-screen">
-        <div className="row justify-content-center">
-          <div className="col bulbasaur-home"></div>
-        </div>
-        <div className="row justify-content-center">
-          <div className="col-8 intro-message"></div>
-        </div>
-      </div>
-      </Router>
+      <>
+      {screen}
+      </>
     );
   }
 }
