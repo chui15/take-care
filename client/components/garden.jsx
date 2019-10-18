d4d39cc51d0a387656dfef149a639487cf07e79d
chui15 <cwsh15@gmail.com>
2019-10-18 22:23:28 +0000

    created garden container and flex-box 4x4 grid

--- client/components/garden.jsx
+++ client/components/garden.jsx
@@ -3,6 +3,15 @@ import { Link } from 'react-router-dom';
 
 class Garden extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.placePlant = this.placePlant.bind(this);
+  }
+
+  placePlant(){
+
+  }
+
   render() {
     let user = localStorage.getItem('UserName');
     return (
@@ -15,6 +24,42 @@ class Garden extends React.Component {
         <div className="row justify-content-center">
           <div className="col-8 header">
             <span className="garden-header-content">{user}'s Garden (^ᴗ^)۶</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-8 ml-2 home-image"></div>
+          <div className="col-md-4 home-message-container align-items-start">
+            <span className="home-message">Hey {user}, tap to view more or plant (◕ᴗ◕✿)</span>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="garden-container">
+            <div className="grid-container">
+              <div className="garden-row">
+                <div className="plant-item-1"></div>
+                <div className="plant-item"></div>
+                <div className="plant-item"></div>
+                <div className="plant-item"></div>
+              </div>
+              <div className="garden-row">
+                <div className="plant-item"></div>
+                <div className="plant-item"></div>
+                <div className="plant-item"></div>
+                <div className="plant-item"></div>
+              </div>
+              <div className="garden-row">
+                <div className="plant-item"></div>
+                <div className="plant-item"></div>
+                <div className="plant-item"></div>
+                <div className="plant-item"></div>
+              </div>
+              <div className="garden-row">
+                <div className="plant-item"></div>
+                <div className="plant-item"></div>
+                <div className="plant-item"></div>
+                <div className="plant-item"></div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
