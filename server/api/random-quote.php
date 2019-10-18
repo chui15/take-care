2bf22fbd2d2105220274029b29e6945019d9ba07
BHninja <bhcodeninja@gmail.com>
2019-10-17 17:31:53 -0700

    added dummy data garden_items goals_list plants random_quote

---
+++ server/api/random-quote.php
@@ -0,0 +1,26 @@
+<?php
+$quotes_array = array(
+  [
+    "id" => 1,
+    "quote" => "What has no fingers, but many rings?
+(A: A tree!)"
+  ],
+  [
+    "id" => "2",
+    "quote" => "What did the big flower say to the little flower?
+(A: What’s up, bud?!)"
+  ],
+  [
+    "id" => "3",
+    "quote" => "How do succulents confess their feelings?
+    (A: Aloe you so much!)"
+  ],
+  [
+    "id" => "4",
+    "quote" => "What makes some plants better at math than others?
+    (A: Square roots!"
+  ]
+  );
+$quotes_array_json = json_encode($quotes_array);
+print $quotes_array
+?>
